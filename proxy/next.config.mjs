cac7cc463d4e01de2d46f055178e172838c833fb
VitalyRK <shishovip@yandex.ru>
2024-05-17 09:25:32 +0300

    feat: set image paths (Fri, May 17, 2024  9:25:32 AM)

--- proxy/next.config.mjs
+++ proxy/next.config.mjs
@@ -1,29 +1,29 @@
-const BASE_URL = "https://api.themoviedb.org/3/";
-
 const API_KEY = process.env.API_KEY;
-
-const FIXED_PARAMS_URL =
-  "discover/movie?include_adult=false&include_video=false&language=en-US";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   async rewrites() {
     return [
       {
-        source: '/movies',
-        destination: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+        source: '/movies/',
+        destination: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc/`,
       },
       {
-        source: '/detail/:id',
-        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&append_to_response=videos&language=en-US`,
+        source: '/movies/:page/',
+        destination: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=:page&sort_by=popularity.desc/`,
       },
       {
-        source: '/img/sm/:path',
-        destination: 'http://image.tmdb.org/t/p/w154/:path',
+        source: '/details/:id/',
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&append_to_response=videos&language=en-US/`,
       },
       {
-        source: '/img/md/:path',
-        destination: 'http://image.tmdb.org/t/p/w342/:path',
+        source: '/img/sm/:path/',
+        destination: 'http://image.tmdb.org/t/p/w154/:path/',
+      },
+      {
+        source: '/img/md/:path/',
+        destination: 'http://image.tmdb.org/t/p/w342/:path/',
       },
     ];
   }
