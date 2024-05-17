d505e2f181c12cb0a2eed0ddbb14229623497213
VitalyRK <shishovip@yandex.ru>
2024-05-17 23:35:12 +0300

    feat: change proxy destinations again (Fri, May 17, 2024 11:35:12 PM)

--- proxy/next.config.mjs
+++ proxy/next.config.mjs
@@ -10,8 +10,8 @@ const nextConfig = {
         destination: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc/`,
       },
       {
-        source: '/movies/:page/:fiters*/',
-        destination: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=:page:fiters*/`,
+        source: '/movies/:page/:year/:sort/:query/:gte/:lte/:genres',
+        destination: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=:page&primary_release_year=:year&sort_by=:sort&with_text_query=:query&vote_average.gte=:gte&vote_average.lte=:lte&with_genres=:genres/`,
       },
       {
         source: '/details/:id/',
